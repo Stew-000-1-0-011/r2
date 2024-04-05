9daee31a058777592a6007a53f0b10c4745eb080
Stew-000-1-0-011 <stew00010011@gmail.com>
2024-04-05 19:54:02 +0900

    collect_debug_node.cppに足回りを追加

---
+++ include/r2/manual_undercarriage_node.hpp
@@ -0,0 +1,56 @@
+#pragma once
+
+#include <cmath>
+#include <utility>
+#include <chrono>
+
+#include <rclcpp/rclcpp.hpp>
+
+#include <sensor_msgs/msg/joy.hpp>
+#include <can_plugins2/msg/frame.hpp>
+
+#include <my_include/xyth.hpp>
+
+#include "robot_config.hpp"
+#include "logicool.hpp"
+#include "shirasu.hpp"
+#include "omni4.hpp"
+
+namespace nhk24_2nd_ws::r2::manual_undercarriage_node::impl {
+	using namespace std::chrono_literals;
+	using xyth::Xyth;
+	using logicool::Axes;
+	using shirasu::Command;
+	using shirasu::command_frame;
+	using shirasu::target_frame;
+	using omni4::Omni4;
+
+	struct ManualUndercarriageNode final : rclcpp::Node {
+		Xyth target;
+		Omni4 omni4;
+
+		rclcpp::Publisher<can_plugins2::msg::Frame>::SharedPtr can_tx;
+		rclcpp::TimerBase::SharedPtr timer;
+		rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr joy;
+
+		ManualUndercarriageNode(const rclcpp::NodeOptions& options = rclcpp::NodeOptions())
+			: rclcpp::Node("manual_undercarriage_node", options)
+			, target(Xyth::zero())
+			, omni4(Omni4::make())
+			, can_tx(this->create_publisher<can_plugins2::msg::Frame>("can_tx", 10))
+			, timer(this->create_wall_timer(10ms, [this]() {
+				const auto speeds = omni4.update(target);
+				for(const auto i : {0, 1, 2, 3}) if(const auto [id, speed] = std::pair{robot_config::ids[i], speeds[i]}; id) can_tx->publish(target_frame(*id, speed));
+			}))
+			, joy(this->create_subscription<sensor_msgs::msg::Joy>("joy", 10, [this](const sensor_msgs::msg::Joy::SharedPtr msg) {
+				target.xy.x = -msg->axes[Axes::l_stick_LR] * robot_config::max_vxy / std::sqrt(2);
+				target.xy.y = msg->axes[Axes::l_stick_UD] * robot_config::max_vxy / std::sqrt(2);
+				target.th = msg->axes[Axes::r_stick_LR] * robot_config::max_vth;
+			}))
+		{}
+	};
+}
+
+namespace nhk24_2nd_ws::r2::manual_undercarriage_node {
+	using impl::ManualUndercarriageNode;
+}
--- src/collect_debug_node.cpp
+++ src/collect_debug_node.cpp
@@ -1,4 +1,7 @@
+#include <rclcpp/rclcpp.hpp>
 #include <r2/collect_debug_node.hpp>
+#include <r2/manual_undercarriage_node.hpp>
+#include <r2/manual_stop_node.hpp>
 
 int main(int argc, char *argv[]) {
 	rclcpp::init(argc, argv);
@@ -6,7 +9,11 @@ int main(int argc, char *argv[]) {
 	auto exec = rclcpp::executors::MultiThreadedExecutor();
 
 	auto collect_debug_node = std::make_shared<nhk24_2nd_ws::r2::collect_debug_node::CollectDebugNode>();
+	auto manual_undercarriage_node = std::make_shared<nhk24_2nd_ws::r2::manual_undercarriage_node::ManualUndercarriageNode>();
+	auto manual_stop_node = std::make_shared<nhk24_2nd_ws::r2::manual_stop_node::ManualStopNode>();
 	exec.add_node(collect_debug_node);
+	exec.add_node(manual_undercarriage_node);
+	exec.add_node(manual_stop_node);
 
 	exec.spin();
 	rclcpp::shutdown();
